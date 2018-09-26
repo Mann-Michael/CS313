8f8442805f6ee295b5e3248a428dc1e33d0547fd
Mann-Michael <uno.mann@gmail.com>
2018-09-25 21:36:58 -0500

    php test

--- web/index.php
+++ web/index.php
@@ -2,15 +2,14 @@
 <html lang="en">
     <head>
         <title>Michael Mann .tech</title>
-		<?php include ("../common/head.php"); ?>
+		<?php /*include ("../common/head.php");*/ ?>
 		
-<!--		<meta charset="utf-8"/>
+		<meta charset="utf-8"/>
 		<link href="../css/style.css" type="text/css" rel="stylesheet" />
 		<script src="../scripts/scripts.js" type="text/javascript"></script>
 		<link href="https://fonts.googleapis.com/css?family=Antic+Slab" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Rock+Salt" rel="stylesheet">
 		
-		-->
     </head>
     <body>
         <header>
