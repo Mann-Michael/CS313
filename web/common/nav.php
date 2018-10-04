731298f871227296a6cc9a5d54ca3d6c458fe25d
Mann-Michael <uno.mann@gmail.com>
2018-10-03 20:56:21 -0500

    edited default includes

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,4 +1,4 @@
 <ul>
-	<li><a href="index.php"><img id="logo" src="images/ui/logo-content.png" alt="Michael Mann .tech Logo" /></a></li>
-	<li class="navbar"><a id="nav-projects" href="index.php?action=about">About</a></li>
+	<li><a href="../index.php"><img id="logo" src="../images/ui/logo-content.png" alt="Michael Mann .tech Logo" /></a></li>
+	<li class="navbar"><a id="nav-projects" href="../index.php?action=about">About</a></li>
 </ul>
