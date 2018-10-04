0c2fe59aa4e2c24e26fe864914d7b32542acc729
Mann-Michael <uno.mann@gmail.com>
2018-10-04 18:54:05 -0500

    FST 3.0

--- web/view/cart-browse.php
+++ web/view/cart-browse.php
@@ -25,8 +25,26 @@
 							array("card4","400"), 
 							array("card5","500")	
 						);
-						
-						
+	
+
+function buildCartBrowseDisplay($products){
+//This area is for building the display of the Cart Browse
+	$length = sizeof($products);
+	$bd = '<ul>';
+	for ($i = 0; $i < $length; $i++) {
+		$bd .= '<li>';
+		$bd .=	$products[$i][0];
+		$bd .= ",";
+		$bd .= $products[$i][1];
+		$bd .=	'</li>';
+	}
+	$bd .= '</ul>';
+
+    return $bd;
+}
+	$displayProd = buildCartBrowseDisplay($products);
+	echo $displayProd;
+/*						
 							$length = sizeof($products);
 							$bd = '<ul>';
 							for ($i = 0; $i < $length; $i++) {
@@ -39,7 +57,7 @@
 							$bd .= '</ul>';
 						echo $bd;
 						
-/*						$length = sizeof($products);
+						$length = sizeof($products);
 					
 						for ($i = 0; $i < $length; $i++) {
 							echo "<li>" . $products[$i][0]. "," . $products[$i][1] . "</li>";
