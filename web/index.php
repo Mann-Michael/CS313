056ae085cc095afd81418273dd56c6de3253ba9d
Mann-Michael <uno.mann@gmail.com>
2018-10-03 20:10:41 -0500

    index MVC

--- web/index.php
+++ web/index.php
@@ -1,29 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Michael Mann .tech</title>
-		<?php include ("common/head.php"); ?>
-    </head>
-    <body>
-        <header>
-        </header>
-        <nav>
-			<?php include ("common/nav.php"); ?>
-			<h1 id="title">CS 313 Assignments</h1>
-        </nav>
-        <main>            
-            <div class="floatpage" id="default" >
-				<p>This is where all the assignment links go.<p>
-				<p>Like this:<p>
-				
-				<ul>
-					<li><a href="/team/3/teamActivity.php">Team Assignment Week 3</a></li>
-					<li><a href="/cart/index.php">Shopping Cart</a></li>
-					<li><a href="#">Link 3</a></li>
-					<li><a href="#">Link 4</a></li>
-			</ul>
-            </div>
-		</main>
-    </body>
-</html>
+<?php
 
+//this is the MICHAELMANN.TECH controller
+
+// Get the functions library
+require_once 'library/functions.php';
+
+$action = filter_input(INPUT_POST, 'action');
+
+if ($action == NULL){
+    $action = filter_input(INPUT_GET, 'action');
+}
+
+switch ($action){
+    case 'something':
+        break;
+    default:
+        include 'view/home.php';
+}
+
+?>
---
+++ web/view/home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Michael Mann .tech</title>
+		<?php include ("common/head.php"); ?>
+    </head>
+    <body>
+        <header>
+        </header>
+        <nav>
+			<?php include ("common/nav.php"); ?>
+			<h1 id="title">CS 313 Assignments</h1>
+        </nav>
+        <main>            
+            <div class="floatpage" id="default" >
+				<ul>
+					<li><a href=".../team/3/teamActivity.php">Team Assignment Week 3</a></li>
+					<li><a href=".../cart/index.php">Shopping Cart</a></li>
+					<li><a href="#">Link 3</a></li>
+					<li><a href="#">Link 4</a></li>
+				</ul>
+            </div>
+		</main>
+    </body>
+</html>
