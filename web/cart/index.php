207ab756c4bd72e6e60570fbf76c89e540398fcd
Mann-Michael <uno.mann@gmail.com>
2018-10-05 21:49:13 -0500

    start to get ready for delete

--- web/cart/index.php
+++ web/cart/index.php
@@ -28,22 +28,26 @@
 				if they want to adjust the cart, they can return to it
 			*/
 			
-			// Fill an array with all products and values from POST
-			$_SESSION["productsReceived"] = array($_POST['product1'], $_POST['product2'], $_POST['product3'], $_POST['product4'], $_POST['product5']);
-			
-			//CReate new array and remove the items that should not be in the cart
-			//1 = Added to cart, 0 = not. 
-			$_SESSION["productsReviewed"] = array();
-			for ($i = 0; $i < count($_SESSION["productsReceived"]); $i++){
-				if ($_SESSION["productsReceived"][$i] == 1) {
-					$_SESSION["productsReviewed"][] = matchProducts($i);
+			//See if there is a products reviewed cart already
+			if isset($_SESSION["productsReviewed"]) {
+				//check to see if an item is being deleted
+				$prodDelete = $_GET['prodDelete'];
+				echo $prodDelete;
+				break;
+				$_SESSION["productsReviewed"] = array_filter($_SESSION["productsReviewed"], 
+			} else {
+				// Fill an array with all products and values from POST
+				$_SESSION["productsReceived"] = array($_POST['product1'], $_POST['product2'], $_POST['product3'], $_POST['product4'], $_POST['product5']);
+				
+				//Create new array and remove the items that should not be in the cart
+				//1 = Added to cart, 0 = not. 
+				$_SESSION["productsReviewed"] = array();
+				for ($i = 0; $i < count($_SESSION["productsReceived"]); $i++){
+					if ($_SESSION["productsReceived"][$i] == 1) {
+						$_SESSION["productsReviewed"][] = matchProducts($i);
+					}
 				}
 			}
-			
-			//check to see if an item is being deleted
-			//$prodDelete = $_GET['prodDelete'];
-			//echo $prodDelete;
-			
 			
 			//Build the view
 			$displayReview = buildCartReviewDisplay($_SESSION["productsReviewed"]);
