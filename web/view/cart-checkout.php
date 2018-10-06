27a9f8d1050f6e65ab854a8e5526469daefdb344
Mann-Michael <uno.mann@gmail.com>
2018-10-06 12:45:35 -0500

    more formatting

--- web/view/cart-checkout.php
+++ web/view/cart-checkout.php
@@ -13,36 +13,26 @@
         </nav>
         <main>            
             <div class="floatpage" id="default" >
-				<form method="post" action="index.php">
-				    <div class="field-wrapper">
+				<div class="field-wrapper">
+					<form method="post" action="index.php">
+				    
                         <label>First Name: </label><br>
                         <input type="text" name="clientFirstName" id="clientFirstName" <?php if(isset($clientFirstName)){echo "value='$clientFirstName'";} ?> required ><br>
-						
                         <label>Last Name: </label><br>
                         <input type="text" name="clientLastName" id="clientLastName" <?php if(isset($clientLastName)){echo "value='$clientLastName'";} ?> required ><br>
-                    </div>
-				    <div class="field-wrapper">
-
-                    </div>
-				    <div class="field-wrapper">
-                        <label>Street Address: </label><br>
+						<label>Street Address: </label><br>
                         <input type="text" name="clientAddress" id="clientAddress" <?php if(isset($clientAddress)){echo "value='$clientAddress'";} ?> required ><br>
-                    </div>
-				    <div class="field-wrapper">
                         <label>City: </label><br>
                         <input type="text" name="clientCity" id="clientCity" <?php if(isset($clientCity)){echo "value='$clientCity'";} ?> required ><br>
-                    </div>
-				    <div class="field-wrapper">
                         <label>State: </label><br>
                         <input type="text" name="clientState" id="clientState" <?php if(isset($clientState)){echo "value='$clientState'";} ?> required ><br>
-                    </div>
-				    <div class="field-wrapper">
                         <label>Zip Code: </label><br>
                         <input type="text" name="clientZip" id="clientZip" <?php if(isset($clientZip)){echo "value='$clientZip'";} ?> required ><br>
-                    </div>					
-				<a href="/cart/index.php?action=viewCartReview"> Back to Review <a>
-				<input type="submit" name="btnConfirm" value="Confirm">
-				<input type="hidden" name="action" value="viewCartConfirm">
+						<a href="/cart/index.php?action=viewCartReview"> Back to Review <a>
+						<input type="submit" name="btnConfirm" value="Confirm">
+						<input type="hidden" name="action" value="viewCartConfirm">
+					</form>
+				</div>
             </div>
 		</main>
     </body>
