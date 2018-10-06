4a314cd3ea3608773b91c5a48de5d836470f63c5
Mann-Michael <uno.mann@gmail.com>
2018-10-06 13:29:02 -0500

    variables showing up properly

--- web/view/cart-checkout.php
+++ web/view/cart-checkout.php
@@ -17,15 +17,15 @@
 					<label>First Name: </label><br>
 					<input type="text" name="clientFirstName" id="clientFirstName" <?php if(isset($_SESSION["clientFirstName"])){echo "value='" . $_SESSION["clientFirstName"] . "'";} ?> required ><br>
 					<label>Last Name: </label><br>
-					<input type="text" name="clientLastName" id="clientLastName" <?php if(isset($clientLastName)){echo "value='$clientLastName'";} ?> required ><br>
+					<input type="text" name="clientLastName" id="clientLastName" <?php if(isset($_SESSION["clientLastName"])){echo "value='" . $_SESSION["clientLastName"] . "'";} ?> required ><br>
 					<label>Street Address: </label><br>
-					<input type="text" name="clientAddress" id="clientAddress" <?php if(isset($clientAddress)){echo "value='$clientAddress'";} ?> required ><br>
+					<input type="text" name="clientAddress" id="clientAddress" <?php if(isset($_SESSION["clientAddress"])){echo "value='" . $_SESSION["clientAddress"] . "'";} ?> required ><br>
 					<label>City: </label><br>
-					<input type="text" name="clientCity" id="clientCity" <?php if(isset($clientCity)){echo "value='$clientCity'";} ?> required ><br>
+					<input type="text" name="clientCity" id="clientCity" <?php if(isset($_SESSION["clientCity"])){echo "value='" . $_SESSION["clientCity"] . "'";} ?> required ><br>
 					<label>State: </label><br>
-					<input type="text" name="clientState" id="clientState" <?php if(isset($clientState)){echo "value='$clientState'";} ?> required ><br>
+					<input type="text" name="clientState" id="clientState" <?php if(isset($_SESSION["clientState"])){echo "value='" . $_SESSION["clientState"] . "'";} ?> required ><br>
 					<label>Zip Code: </label><br>
-					<input type="text" name="clientZip" id="clientZip" <?php if(isset($clientZip)){echo "value='$clientZip'";} ?> required ><br>
+					<input type="text" name="clientZip" id="clientZip" <?php if(isset($_SESSION["clientZip"])){echo "value='" . $_SESSION["clientZip"] . "'";} ?> required ><br>
 					<a href="/cart/index.php?action=viewCartReview"> Back to Review <a>
 					<input type="submit" name="btnConfirm" value="Confirm">
 					<input type="hidden" name="action" value="viewCartConfirm">
