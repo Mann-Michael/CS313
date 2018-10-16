3aba60534e090f05fa9dd9b0017bb236228dfc56
Mann-Michael <uno.mann@gmail.com>
2018-10-15 19:35:40 -0500

    dbConnect1

---
+++ db/myDB.sql
@@ -0,0 +1,25 @@
+CREATE TABLE swimmer (
+id SERIAL PRIMARY KEY, 
+name VARCHAR (255) NOT NULL, 
+age INT, 
+gender BOOLEAN NOT NULL, 
+team VARCHAR (255) NOT NULL, 
+email VARCHAR (255) NOT NULL, 
+password VARCHAR (255) NOT NULL
+);
+CREATE TABLE event (
+id SERIAL PRIMARY KEY,
+strokeid INT REFERENCES stroke(id), 
+distanceid INT REFERENCES distance(id),
+time FLOAT (3) NOT NULL,
+location VARCHAR (255) NOT NULL,
+date DATE NOT NULL
+);
+CREATE TABLE stroke (
+id SERIAL PRIMARY KEY, 
+stroketype VARCHAR (255) NOT NULL
+);
+CREATE TABLE distance (
+id SERIAL PRIMARY KEY, 
+distance INT NOT NULL
+);
