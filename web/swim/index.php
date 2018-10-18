7afabcebce2aa020733e15c59d12c92720885e93
Mann-Michael <uno.mann@gmail.com>
2018-10-17 19:53:31 -0500

    Update web/swim/index.php

--- web/swim/index.php
+++ web/swim/index.php
@@ -11,7 +11,39 @@
 	//Get the swim model
     require_once '../model/swim-model.php';
 
-	//Create general information - UNUSED 
+	//Database Connection
+	function dbConnect(){
+		try{
+			$dbUrl = getenv('DATABASE_URL');
+			$dbOpts = parse_url($dbUrl);
+			$dbHost = $dbOpts["host"];
+			$dbPort = $dbOpts["port"];
+			$dbUser = $dbOpts["user"];
+			$dbPassword = $dbOpts["pass"];
+			$dbName = ltrim($dbOpts["path"],'/');
+
+			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $db;
+		}
+		catch (PDOException $ex){
+			echo 'Error!: ' . $ex->getMessage();
+			die();
+		}
+	}
+	dbConnect();
+	
+	//Model Information
+	
+	//Get all swimmers
+		$db = dbConnect();
+		$stmt = $db->prepare('SELECT * FROM swimmer');
+		$stmt->execute();
+		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	//Get single swimmer profile
+	//Get top 3 swim times per event
+	
 	
 	//Get $action, if $action is null then set it to default
     $action = filter_input(INPUT_POST, 'action');
@@ -25,48 +57,24 @@
 			break;
 		default:
 		
-			try
-			{
-			  $dbUrl = getenv('DATABASE_URL');
-
-			  $dbOpts = parse_url($dbUrl);
-
-			  $dbHost = $dbOpts["host"];
-			  $dbPort = $dbOpts["port"];
-			  $dbUser = $dbOpts["user"];
-			  $dbPassword = $dbOpts["pass"];
-			  $dbName = ltrim($dbOpts["path"],'/');
-
-			  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-			  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			}
-			catch (PDOException $ex)
-			{
-			  echo 'Error!: ' . $ex->getMessage();
-			  die();
-			}
 			
-			$stmt = $db->prepare('SELECT * FROM swimmer');
-			$stmt->execute();
-			$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			
+
 			
 			print_r($rows);			
 			$swimmers = $rows;
-            if(count($swimmers) > 0){
+            if(count($rows) > 0){
 				echo "swimmers > 0 ";
                 $swimmerList = '<table>';
                 $swimmerList .= '<thead>';
                 $swimmerList .= '<tr><th>Swimmer</th><td>&nbsp;</td><td>&nbsp;</td></tr>';
                 $swimmerList .= '</thead>';
                 $swimmerList .= '<tbody>';
-                foreach ($swimmers as $swimmer) {
-					$swimmerList .= "<tr><td>$swimmer[name]</td>";
+                foreach ($rows as $row) {
+					$swimmerList .= "<tr><td>$row['name']</td>";
                 }
 					$swimmerList .= '</tbody></table>';
                 } else {
-					$count = count($swimmers);
-					echo $count;
 					$swimmerList = "<p class='notify'>Sorry, no swimmers were returned.</p>";
                 }
 		
