4cef0a830520fef44e72e5ae4fa51f756facb19d
Mann-Michael <uno.mann@gmail.com>
2018-10-26 18:20:16 -0500

    Update web/view/swim-editswimmer.php

--- web/view/swim-editswimmer.php
+++ web/view/swim-editswimmer.php
@@ -43,8 +43,8 @@
 					</div>
 					<div class="field-wrapper">
 						<label>Gender</label>
-						<input type="radio" name="swimmerGender" value="0" <?php if(isset($swimmerGender == 0)){ echo 'checked="checked"'; }?>>Female</input>
-						<input type="radio" name="swimmerGender" value="1"<?php if(isset($swimmerGender == 1)){ echo 'checked="checked"'; }?>>Male</input>
+						<input type="radio" name="swimmerGender" value="0" <?php if(isset($swimmerGender) && ($swimmerGender == 0)){ echo 'checked="checked"'; }?>>Female</input>
+						<input type="radio" name="swimmerGender" value="1" <?php if(isset($swimmerGender) && ($swimmerGender == 1)){ echo 'checked="checked"'; }?>>Male</input>
 						
 						<input type="text" name=swimmerGender" <?php if(isset($swimmerGender)){ echo "value='$swimmerGender'"; }?>>
 					</div>
