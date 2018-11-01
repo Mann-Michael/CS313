17ef430fdd9a4bb485570452a8923aa6afef7a34
Mann-Michael <uno.mann@gmail.com>
2018-10-31 20:06:57 -0500

    Update web/view/swim-profile.php

--- web/view/swim-profile.php
+++ web/view/swim-profile.php
@@ -36,8 +36,6 @@
             <div class="floatpage" id="default">
 				<?php 
 				
-				echo $_SESSION['id'];
-				echo $swimmerId;
 					//If user is logged in and this is their profile, then build add event and edit profile
 					if($_SESSION['loggedin'] == TRUE && $_SESSION['id'] == $swimmerId) {
 						echo '<p><a href="index.php?action=viewEditSwimmer">Edit Swimmer</a><span>||</span><a href="index.php?action=viewAddEvent">Add Event</a></p>';
